ce13b14354f85bd67ca9dbfcdf91202c69104f9e
Mamus Omokiti <mamusomokiti@Mamuss-MacBook-Pro.local>
2024-09-10 19:23:49 +0100

    change ui

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+       <h2>Welcome to my website</h2>
+       
+      </header>
+      <div className='links'>
+        <div className='linkz'>
+       
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Home
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          About
+          
         </a>
-      </header>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact
+          
+        </a>
+      </div>
+         
+      </div>
     </div>
   );
 }
