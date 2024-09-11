6f0f6f2f305fff2d5b875ae4434a0b191b391eec
Mamus Omokiti <mamusomokiti@Mamuss-MacBook-Pro.local>
2024-09-11 16:01:35 +0100

    secondcommit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Intercom from '@intercom/messenger-js-sdk';
 
+Intercom({
+  app_id: 'befoprcf',
+  // user_id: user.id, // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
+  // name: user.name, // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
+  // email: user.email, // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
+  // created_at: user.createdAt, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date in a Unix timestamp (in seconds) e.g. 1704067200
+});
 function App() {
   return (
     <div className="App">
